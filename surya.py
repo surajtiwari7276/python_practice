d9e4248c92db7f4263b6c18b897cdee99abf3e30
surajtiwari7276 <46623273+surajtiwari7276@users.noreply.github.com>
2019-01-13 13:06:06 +0530

    Add files via upload
    13-01-2019

---
+++ surya.py
@@ -0,0 +1,15 @@
+a=int(input ("what is  the side of square"))
+b=a**2
+print(f"side of{a}square is {b} ")
+
+
+
+c=int(input("what is radius of a circle"))
+d=3.14*(c**2)
+print(f"area of circle {d} ")
+
+
+e=int(input("what is length  of rectangle"))
+f=int(input("what is breadth  of rectangle"))
+h=e*f
+print(f"area of rectangle{h} ")
